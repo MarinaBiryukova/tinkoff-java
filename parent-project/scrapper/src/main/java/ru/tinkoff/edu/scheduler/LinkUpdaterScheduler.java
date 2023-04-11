235a0e67dbebd019831d305a41d78e287833e3a4
MarinaBiryukova <m.i.biryukova@gmail.com>
2023-04-11 23:23:57 +0300

    Added jooq link updater

--- parent-project/scrapper/src/main/java/ru/tinkoff/edu/scheduler/LinkUpdaterScheduler.java
+++ parent-project/scrapper/src/main/java/ru/tinkoff/edu/scheduler/LinkUpdaterScheduler.java
@@ -1,19 +1,17 @@
 package ru.tinkoff.edu.scheduler;
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import ru.tinkoff.edu.repository.LinkRepository;
 import ru.tinkoff.edu.repository.dto.Link;
-import ru.tinkoff.edu.service.LinkUpdater;
-
+import ru.tinkoff.edu.service.jdbc.JdbcLinkUpdater;
 import java.util.List;
 
 @Component
 @AllArgsConstructor
 public class LinkUpdaterScheduler {
-    private final LinkUpdater linkUpdater;
+    private final JdbcLinkUpdater linkUpdater;
     private final LinkRepository linkRepository;
 
     @Scheduled(fixedDelayString = "#{@applicationConfig.scheduler.interval()}")
@@ -22,7 +20,6 @@ public class LinkUpdaterScheduler {
 
         List<Link> links = linkRepository.findAllForUpdate();
         for (Link link: links) {
-            System.out.println("Updating link" + link.getLink());
             linkUpdater.update(link);
         }
 
--- parent-project/scrapper/src/main/java/ru/tinkoff/edu/service/jooq/JooqLinkUpdater.java
+++ parent-project/scrapper/src/main/java/ru/tinkoff/edu/service/jooq/JooqLinkUpdater.java
@@ -1,16 +1,16 @@
-package ru.tinkoff.edu.service.jdbc;
+package ru.tinkoff.edu.service.jooq;
 
 import lombok.AllArgsConstructor;
+import org.jooq.DSLContext;
 import org.springframework.stereotype.Service;
 import ru.tinkoff.edu.client.BotClient;
 import ru.tinkoff.edu.client.GitHubClient;
 import ru.tinkoff.edu.client.StackOverflowClient;
+import ru.tinkoff.edu.domain.jooq.Tables;
 import ru.tinkoff.edu.parser.GitHubLinkParser;
 import ru.tinkoff.edu.parser.StackOverflowLinkParser;
 import ru.tinkoff.edu.record.GitHubRecord;
 import ru.tinkoff.edu.record.StackOverflowRecord;
-import ru.tinkoff.edu.repository.ChatLinkRepository;
-import ru.tinkoff.edu.repository.LinkRepository;
 import ru.tinkoff.edu.repository.dto.Link;
 import ru.tinkoff.edu.repository.dto.TgChat;
 import ru.tinkoff.edu.request.LinkUpdate;
@@ -24,14 +24,13 @@ import java.util.List;
 
 @AllArgsConstructor
 @Service
-public class JdbcLinkUpdater implements LinkUpdater {
-    private final ChatLinkRepository chatLinkRepository;
-    private final LinkRepository linkRepository;
+public class JooqLinkUpdater implements LinkUpdater {
     private final BotClient botClient;
     private final GitHubClient gitHubClient;
     private final StackOverflowClient stackOverflowClient;
     private final GitHubLinkParser gitHubLinkParser = new GitHubLinkParser(null);
     private final StackOverflowLinkParser stackOverflowLinkParser = new StackOverflowLinkParser(gitHubLinkParser);
+    private DSLContext context;
 
     @Override
     public void update(Link link) {
@@ -42,31 +41,31 @@ public class JdbcLinkUpdater implements LinkUpdater {
             if (!response.answer_count().equals(link.getAnswerCount())) {
                 link.setLastActivity(response.last_activity_date());
                 link.setAnswerCount(response.answer_count());
-                List<Long> chatIds = chatLinkRepository.getChatsForLink(link).stream().map(TgChat::getTgChatId).toList();
+                List<Long> chatIds = getChatsForLink(link).stream().map(TgChat::getTgChatId).toList();
                 sendUpdate(link, "In question '" + response.question_id() + "' new answers have been added!", chatIds);
             } else if (response.last_activity_date().isAfter(link.getLastActivity())) {
                 link.setLastActivity(response.last_activity_date());
-                List<Long> chatIds = chatLinkRepository.getChatsForLink(link).stream().map(TgChat::getTgChatId).toList();
+                List<Long> chatIds = getChatsForLink(link).stream().map(TgChat::getTgChatId).toList();
                 sendUpdate(link, "Question '" + response.question_id() + "' has updates!", chatIds);
             }
-            linkRepository.update(link);
+            updateLink(link);
         } else if (record instanceof GitHubRecord) {
             RepositoryResponse response = gitHubClient.getRepoInfo(((GitHubRecord) record).username(), ((GitHubRecord) record).repo());
             link.setLastUpdate(OffsetDateTime.now());
             if (!response.open_issues_count().equals(link.getOpenIssuesCount())) {
                 link.setLastActivity(response.updated_at());
                 link.setOpenIssuesCount(response.open_issues_count());
-                List<Long> chatIds = chatLinkRepository.getChatsForLink(link).stream().map(TgChat::getTgChatId).toList();
+                List<Long> chatIds = getChatsForLink(link).stream().map(TgChat::getTgChatId).toList();
                 String desc = (response.open_issues_count() > link.getOpenIssuesCount()) ?
                         "In repository '" + response.full_name() + "' new open issues have been added!" :
                         "In repository '" + response.full_name() + "' issues have been closed!";
                 sendUpdate(link, desc, chatIds);
             } else if (response.updated_at().isAfter(link.getLastActivity())) {
                 link.setLastActivity(response.updated_at());
-                List<Long> chatIds = chatLinkRepository.getChatsForLink(link).stream().map(TgChat::getTgChatId).toList();
+                List<Long> chatIds = getChatsForLink(link).stream().map(TgChat::getTgChatId).toList();
                 sendUpdate(link, "Repository '" + response.full_name() + "' has updates!", chatIds);
             }
-            linkRepository.update(link);
+            updateLink(link);
         }
     }
 
@@ -77,5 +76,18 @@ public class JdbcLinkUpdater implements LinkUpdater {
         request.setDescription(description);
         request.setTgChatIds(tgChatIds);
         botClient.sendUpdate(request);
+    }
+
+    private List<TgChat> getChatsForLink(Link link) {
+        return context.select(Tables.CHAT.fields()).from(Tables.CHAT_LINK).join(Tables.CHAT).on(Tables.CHAT.ID.eq(Tables.CHAT_LINK.CHAT_ID))
+                .where(Tables.CHAT_LINK.LINK_ID.eq(link.getId())).fetchInto(TgChat.class);
+    }
+
+    private void updateLink(Link link) {
+        context.update(Tables.LINK).set(Tables.LINK.LAST_UPDATE, link.getLastUpdate().toLocalDateTime())
+                .set(Tables.LINK.LAST_ACTIVITY, link.getLastActivity().toLocalDateTime())
+                .set(Tables.LINK.ANSWER_COUNT, link.getAnswerCount())
+                .set(Tables.LINK.OPEN_ISSUES_COUNT, link.getOpenIssuesCount())
+                .where(Tables.LINK.ID.eq(link.getId())).execute();
     }
 }
